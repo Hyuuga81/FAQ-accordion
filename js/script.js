369de4ba7ea9becbbe2282db65a754a9ca325f7d
Jay <j.adeyina@gmail.com>
2024-05-03 08:02:54 +0900

    Updated function in js

--- js/script.js
+++ js/script.js
@@ -1,21 +1,24 @@
-const qAndA = document.querySelectorAll('.qna');
+document.addEventListener('DOMContentLoaded', function () {
+        const qAndA = document.querySelectorAll('.qna');
 
-qAndA.forEach((item)=> {
-    const plus = item.querySelector('.qna .q');
-    const icon = item.querySelector('.qna .q img');
-    const paragraph = item.querySelector('.qna p');
+        qAndA.forEach((item)=> {
+        const plus = item.querySelector('.qna .q');
+        const icon = item.querySelector('.qna .q img');
+        const paragraph = item.querySelector('.qna p');
 
-    plus.addEventListener('click', () => {
-        console.log('clicked');
-        if (paragraph.classList.contains('hide')) {
-            paragraph.classList.remove('hide');
-            paragraph.style.width = "100%";
-            icon.src = "../img/icon-minus.svg";
-            icon.alt = "minus";
-        } else {
-            paragraph.classList.add('hide');
-            icon.src = "../img/icon-plus.svg";
-            icon.alt = "plus";
-        }
+        plus.addEventListener('click', () => {
+            console.log('clicked');
+            if (paragraph.classList.contains('hide')) {
+                paragraph.classList.remove('hide');
+                paragraph.style.width = "100%";
+                icon.src = "../img/icon-minus.svg";
+                icon.alt = "minus";
+            } else {
+                paragraph.classList.add('hide');
+                icon.src = "../img/icon-plus.svg";
+                icon.alt = "plus";
+            }
+        });
     });
+
 });
